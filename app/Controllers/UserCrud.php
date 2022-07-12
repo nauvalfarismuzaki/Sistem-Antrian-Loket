26861eadca5543ae6b12a874e4627a0d7303e0eb
nauvalfarismuzaki <nauvalmuzaki00@gmail.com>
2022-07-13 01:22:56 +0700

    Update File CI4

---
+++ app/Controllers/UserCrud.php
@@ -0,0 +1,60 @@
+<?php 
+namespace App\Controllers;
+use App\Models\UserModel;
+use CodeIgniter\Controller;
+class UserCrud extends Controller
+{
+    // show users list
+    public function index(){
+        $userModel = new UserModel();
+        $data['users'] = $userModel->orderBy('id', 'DESC')->findAll();
+        return view('user_view', $data);
+    }
+    // add user form
+    public function create(){
+        return view('add_user');
+    }
+ 
+    // insert data
+    public function store() {
+        $userModel = new UserModel();
+        $data = [
+            'name' => $this->request->getVar('name'),
+            'email'  => $this->request->getVar('email'),
+        ];
+        $userModel->insert($data);
+        return $this->response->redirect('/loket/antri/public/users-list');
+    }
+    // show single user
+    public function singleUser($id = null){
+        $userModel = new UserModel();
+        $data['user_obj'] = $userModel->where('id', $id)->first();
+        return view('edit_view', $data);
+    }
+    // update user data
+    public function update(){
+        $userModel = new UserModel();
+        $id = $this->request->getVar('id');
+        $data = [
+            'name' => $this->request->getVar('name'),
+            'email'  => $this->request->getVar('email'),
+        ];
+        $userModel->update($id, $data);
+        return $this->response->redirect('/loket/antri/public/users-list');
+    }
+ 
+    // delete user
+    public function delete($id = null){
+        $userModel = new UserModel();
+        $data['user'] = $userModel->where('id', $id)->delete($id);
+        return $this->response->redirect('/loket/antri/public/users-list');
+    }    
+    
+    //cetak
+    public function print($id = null)
+    {
+      $userModel = new UserModel();
+      $data['user_obj'] = $userModel->where('id', $id)->first();
+      return view('cetak', $data);
+    }
+}
